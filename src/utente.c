b86af5b88e9bf4794d3ae000604087a70b1e813e
Shugaaar <massi26mcc@gmail.com>
2026-04-27 18:21:37 +0200

    continuato lo sviluppo di utente.c

--- src/utente.c
+++ src/utente.c
@@ -38,6 +38,7 @@ int main(int argc,char* argv[]){
 
     if(bind(sockfd,(struct sockaddr*)&my_addr,sizeof(my_addr))<0){
         perror("Errore nella bind");
+        close(sockfd);
         return 1;
     }
     
@@ -58,7 +59,99 @@ int main(int argc,char* argv[]){
 
     printf("Utente porta %d: Registrato alla lavagna.\n", my_port);
 
+    int costs[MAX_UTENTI-1]; //costi ricevuti dagli altri utenti
+    int n_peer_remaining; //counter di peer da cui ricevere ancora il costo
+    int n_peer; //numero di peer da contattare dopo AVAILABLE_CARD
+    int my_cost; //costo calcolato randomicamente
+
+    Card doing; //se id!=-1 corrisponde alla carta che sto eseguendo
+
     while(1){ //loop principale, attendo che arrivi un pacchetto e agisco di conseguenza
+
+        Packet rcv_pkt;
+        struct sockaddr_in sender_addr;
+        socklen_t addr_len = sizeof(sender_addr);
+
+        //aspetto la ricezione di un pacchetto
+        if(recvfrom(sockfd,&rcv_pkt,sizeof(Packet),0,(struct sockaddr*)&sender_addr,&addr_len)<0){
+            perror("Errore nella ricezione del pacchetto");
+            close(sockfd);
+            return 1;
+        }
+
+        switch(rcv_pkt.cmd){
+
+            case AVAILABLE_CARD:
+
+                printf("Ricevuta card disponibile, id:%i",rcv_pkt.card.id);
+                
+                copy_card(rcv_pkt.card,doing);//salvo i dati della carta
+                srand(time(NULL));
+                my_cost=rand(); //genero il costo randomico
+
+                //creo il pacchetto con il mio costo
+                Packet cost_pkt;
+                cost_pkt.cmd=CHOOSE_USER;
+                cost_pkt.cost=my_cost;
+
+                struct sockaddr_in host_addr;
+                host_addr.sin_family=AF_INET;
+                host_addr.sin_addr.s_addr=INADDR_ANY;
+                n_peer_remaining=rcv_pkt.n_users; // aggiorno il counter di costi da ricevere
+
+                for(int i=0;i<rcv_pkt.n_users;i++){ //spedisco il costo ai vari peer
+                    host_addr.sin_port=rcv_pkt.users_ports[i];
+                    send_packet(sockfd,&cost_pkt,&host_addr);
+                }
+
+            case CHOOSE_USER:
+
+                costs[n_peer-n_peer_remaining]=rcv_pkt.cost; //salvo il costo ricevuto
+                n_peer_remaining--;
+                int chosen=1;
+                if(!n_peer_remaining){ 
+                    for(int i=0;i<n_peer;i++){ //se il mio costo è maggiore di almeno un peer non sono il chosen
+                        if(my_cost>costs[i]){
+                            chosen=0;
+                            doing.id=-1;
+                        }
+                    }
+                    if(chosen){ // se sono il chosen mando alla lavagna l'ACK_CARD
+
+                        printf("Ti è stata assegnata una carta\n");
+                        print_card(doing);
+
+                        Packet ack_pkt;
+                        ack_pkt.cmd=ACK_CARD;
+                        send_packet(sockfd,&ack_pkt,&server_addr);
+
+                        //una volta ricevuto il task abilito il terminale all'host in modo che possa segnalare un CARD_DONE o un QUIT alla lavagna
+
+                        while(1){
+                            char cmd[]=scanf("Inserire un comando:\n");
+                            if(strcmp(cmd,"CARD_DONE")){
+                                Packet done_pkt;
+                                done_pkt.cmd=CARD_DONE;
+                                send_packet(sockfd,&done_pkt,&server_addr);
+                                break;
+
+                            }else if(strcmp(cmd,"QUIT")){
+                                Packet quit_pkt;
+                                quit_pkt.cmd=QUIT;
+                                send_packet(sockfd,&quit_pkt,&server_addr);
+
+                                close(sockfd);
+                                return 0;
+                            }else{
+                                printf("Comando non valido...\n");
+                            }
+                        }        
+                    }
+                }
+        }
+
+
+
 
     }
 
--- src/utils.c
+++ src/utils.c
@@ -1,6 +1,41 @@
 #include "../include/protocol.h"
+#include <string.h>
 
 //funzione generica condivisa tra server e client per inviare un pacchetto
 void send_packet(int socket,Packet *p,struct sockaddr_in *dest){
-    sendto(socket,p,sizeof(Packet),0,(struct sockaddr*) dest);
+    sendto(socket,p,sizeof(Packet),0,(struct sockaddr*) dest,sizeof(dest));
+}
+
+//copia la carta src in dst
+void copy_card(Card src,Card dst){
+    dst.col=src.col;
+    dst.id=src.id;
+    dst.next=src.next;
+    strcpy(dst.text,src.text);
+    dst.timestamp=src.timestamp;
+    dst.user_port=src.user_port;
+}
+//fornisce una rappresentazione grafica della carta
+void print_card(Card c){ 
+    int len=strlen(c.text);
+    for(int i=0;i<24;i++){
+        printf("-");
+    }
+    printf("\n");
+    printf("ID: %i\n\n",c.id);
+    printf('"');
+    for(int i=0;i<len;i++){
+        if(i%20==0){
+            printf("-  ");
+            printf("\n");
+            printf(". -");
+
+        }
+        printf("%c",c.text[i]);
+    }
+    printf('"\n');
+    for(int i=0;i<24;i++){
+        printf("-");
+    }
+    printf("\n\n");
 }
